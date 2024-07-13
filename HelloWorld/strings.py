741fb25fd54558eba7084e5594dda161c12b4138
Bevanwhite <jefferybevan@gmail.com>
2024-07-13 13:04:48 +0530

    finished up to escape character

---
+++ HelloWorld/strings.py
@@ -0,0 +1,12 @@
+print("Today is a good day to learn python")
+print('Python is fun')
+print("Python's string are easy to use")
+print('we can even include "quotes" in strings')
+# string concatenation
+print("hello" + "World")
+greeting = "Hello"
+name = input("Please enter your name ")
+print(greeting + name)
+
+# if we want a space, we can add that too
+print(greeting + ' ' + name)
