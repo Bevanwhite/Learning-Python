249a47ab7f44c0a771fe9085db481e2f621760df
Bevanwhite <jefferybevan@gmail.com>
2025-04-07 16:50:13 +0530

    printing in python

--- HelloWorld/HelloWorld.py
+++ HelloWorld/HelloWorld.py
@@ -1,1 +1,7 @@
-print('Hello World')
+print('Hello, World!')  # you can use double quotes too. but it has to be the same.
+#  we are passing string argument for the print function
+
+print(1 + 2)
+print(7 * 6)
+print()
+print("The end", "or is it?", "keep watching to learn more about Python", 3)
